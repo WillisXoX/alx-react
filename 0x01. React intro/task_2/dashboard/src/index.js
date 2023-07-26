71b4361e39a40fb58c15783bf12c6e6abe5b2614
Willis <Williamstone4041@gmail.com>
2023-07-26 14:36:50 +0100

    React intro

---
+++ 0x01. React intro/task_2/dashboard/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+
+import Notifications from "./Notifications";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+reportWebVitals();
